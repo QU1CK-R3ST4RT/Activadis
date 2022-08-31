9f2242fa49580370dc042d737cc0d3f1690ad5de
dkempenk <dionkempenk@gmail.com>
2022-08-31 20:17:03 +0200

    laravel init

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        colors: {
+            "project_dec": "#6B6B6B",
+            "packageInfo": "#DFDFDF",
+            "wl_green": {
+                "100": "#ECFEFF",
+                "200": "#A8F9FD",
+                "300": "#3ED9DE",
+                "400": "#32B3B8",
+                "500": "#299A9E",
+                "600": "#1F7B7F",
+                "700": "#166063",
+                "800": "#0D4446",
+                "900": "#062E2F",
+                "dark": "#021A1B",
+            },
+        },
+        extend: {},
+    },
+    plugins: [],
+}
