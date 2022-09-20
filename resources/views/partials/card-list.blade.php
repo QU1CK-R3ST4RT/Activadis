1093d0a97346f46cd9e2f6ddf9891d3c1fdf526b
Wlodek <gebicki.wlodek@gmail.com>
2022-09-20 20:47:39 +0200

    Scrollable list
    added a scrollable division with the ability to add multiple items within.

---
+++ resources/views/partials/card-list.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.layout')
+
+
+
+@section('content')
+<div class="h-[650px] w-[575px] ml-[300px] mt-[100px] overflow-y-scroll flex justify-center flex-wrap">
+    
+    {{-- @foreach($cards as $card)
+        @include('components.card')
+    @endforeach --}}
+
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div> 
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div> 
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
+    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>    
+    
+</div>
+
+@endsection
