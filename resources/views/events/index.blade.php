e58ed9a680e33e34f86e76e5160b959e4c5e7db3
Dion Kempenk <36001957+QU1CK-R3ST4RT@users.noreply.github.com>
2022-09-21 13:27:16 +0200

    Delete index.blade.php

--- resources/views/events/index.blade.php
+++
@@ -1,9 +0,0 @@
-<body>
-    <h1>Overzicht van alle evenementen</h1>
-    <p>Bekijk hier een overzicht van alle evenementen.</p>
-    <br>
-
-    @foreach($events as $event)
-        <kbd><a href="/events/{!! $event->id !!}">{!! $event['id'] !!}</a> | Word in <a href="/events/locatie/{!! $event->location !!}">{!! $event['location'] !!}</a> gehouden.</kbd> <br>
-    @endforeach
-</body>
