f3959f36a6e879f61a97461fbad6c21f3e66e54f
Wlodek <gebicki.wlodek@gmail.com>
2022-09-21 13:23:12 +0200

    Refined

--- resources/views/partials/card-list.blade.php
+++ resources/views/partials/card-list.blade.php
@@ -3,36 +3,27 @@
 
 
 @section('content')
-<div class="h-[650px] w-[575px] ml-[300px] mt-[100px] overflow-y-scroll flex justify-center flex-wrap">
+<div class="bg-red-500 h-[650px] w-screen overflow-y-scroll flex justify-center ">
     
     {{-- @foreach($cards as $card)
         @include('components.card')
     @endforeach --}}
-
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[15px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div> 
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[20px]"></div> 
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>
-    <div class="bg-green-500 h-[60px] w-[540px] mt-[25px]"></div>    
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
+        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
     
 </div>
 
