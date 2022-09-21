0fc259d6ab7937fd806e8dc35fb2415f21ac168f
Wlodek <gebicki.wlodek@gmail.com>
2022-09-21 15:23:27 +0200

    Fixed positioning

--- resources/views/partials/card-list.blade.php
+++ resources/views/partials/card-list.blade.php
@@ -3,28 +3,33 @@
 
 
 @section('content')
-<div class="bg-red-500 h-[650px] w-screen overflow-y-scroll flex justify-center ">
-    
-    {{-- @foreach($cards as $card)
-        @include('components.card')
-    @endforeach --}}
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-        <div class="bg-green-500 h-[60px] w-screen mt-[15px] ml-[15px]"></div>
-    
-</div>
+    <div class="grid grid-cols-2 mt-10">
+        <div class="overflow-y-scroll h-[650px]">
+            <div class="w-full">
+                @foreach (\App\Models\Event::all() as $e)
+                    @component('components.single-activity')
+                        @slot('color')
+                            {{ $e->color }}
+                        @endslot
 
+                        @slot('day')
+                            MA
+                        @endslot
+
+                        @slot('title')
+                            {{ $e->name }}
+                        @endslot
+
+                        @slot('date')
+                            {{ $e->start_time}}
+                        @endslot
+
+                        @slot('time')
+                            {{ $e->start_time }}
+                        @endslot
+                    @endcomponent
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
