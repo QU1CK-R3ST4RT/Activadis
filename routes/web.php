de3aced21c26775867bb317d36ea9dccd028a85b
ProdMelts <nazar.sarkisjan@student.graafschapcollege.nl>
2022-10-03 14:25:22 +0200

    Created Login View Updated Event Modal Variables

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login-modal');
+});
+
+Route::get('/event', function () {
+    return view('event-modal');
+});
