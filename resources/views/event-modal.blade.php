b22fcde99e9ecfb0140799e9813523192aea1a86
RikkertTheDeveloper <rejarendsen@gmail.com>
2022-10-05 09:55:20 +0200

    Update Routes, Add EventController.php Functions

--- resources/views/event-modal.blade.php
+++ resources/views/event-modal.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.layout')
 
-@section('content')
     <div class="outer-container">
         <div class="main-container">
             <div class="card-banner"></div>
@@ -8,52 +7,51 @@
                 <table>
                     <tr>
                         <td>
-                            <input type="text" placeholder="Event name" class="textfields">
+                            <input type="text" placeholder="Event name" class="textfields" value={{$name ?""}}>
                         </td>
                     </tr> 
                     <tr>
                         <td>
-                            <input type="text" placeholder="Location" class="textfields">
+                            <input type="text" placeholder="Location" class="textfields" value={{$location ?""}}>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <label for="datePicker"></label>
-                            <input type="date" name="datePicker" class="textfields">
+                            <input type="date" name="datePicker" class="textfields" value={{$date ?""}}>
                         </td>
                     </tr>
                     <tr>
                         <td>
                             <label for="beginTime">Starting at</label>
-                            <input type="time" name="beginTime" placeholder="" class="textfields">
+                            <input type="time" name="beginTime" placeholder="" class="textfields" value={{$start_time ?""}}>
                             <label for="endTime">Ending at</label>
-                            <input type="time" name="endTime" placeholder="" class="textfields">
+                            <input type="time" name="endTime" placeholder="" class="textfields" value={{$end_time ?""}}>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <textarea type="text" placeholder="Discription" class="description-textfield"></textarea>
+                            <textarea type="text" placeholder="Description" class="description-textfield" value={{$description ?""}}></textarea>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <input type="checkbox" name="foodIncluded" placeholder="0" class="textfields">
+                            <input type="checkbox" name="foodIncluded" placeholder="0" class="textfields" value={{$food_included ?false}}>
                             <label for="foodIncluded">Food Included</label>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                            <input type="number" min="0" placeholder="Pricing" class="textfields">
+                            <input type="number" min="0" placeholder="Pricing" class="textfields" value={{$price ?""}}>
                         </td> 
                     </tr>
                     <tr>
                         <td>
                             <label for="colorPicker">Card color</label>
-                            <input type="color" name="colorPicker" class="textfields">
+                            <input type="color" name="colorPicker" class="textfields" value={{$color ?""}}>
                         </td>
                     </tr>
                 </table>
             </div>
         </div>
     </div>
-@endsection
--- resources/views/partials/card-list.blade.php
+++ resources/views/partials/card-list.blade.php
@@ -1,35 +1,27 @@
-@extends('layouts.layout')
+    <div class="overflow-y-scroll h-[650px]">
+        <div class="w-full">
+            @foreach (\App\Models\Event::all() as $e)
+                @component('components.single-activity')
+                    @slot('color')
+                        {{ $e->color }}
+                    @endslot
 
+                    @slot('day')
+                        MA
+                    @endslot
 
+                    @slot('title')
+                        {{ $e->name }}
+                    @endslot
 
-@section('content')
-    <div class="grid grid-cols-2 mt-10">
-        <div class="overflow-y-scroll h-[650px]">
-            <div class="w-full">
-                @foreach (\App\Models\Event::all() as $e)
-                    @component('components.single-activity')
-                        @slot('color')
-                            {{ $e->color }}
-                        @endslot
+                    @slot('date')
+                        {{ $e->start_time }}
+                    @endslot
 
-                        @slot('day')
-                            MA
-                        @endslot
-
-                        @slot('title')
-                            {{ $e->name }}
-                        @endslot
-
-                        @slot('date')
-                            {{ $e->start_time}}
-                        @endslot
-
-                        @slot('time')
-                            {{ $e->start_time }}
-                        @endslot
-                    @endcomponent
-                @endforeach
-            </div>
+                    @slot('time')
+                        {{ $e->start_time }}
+                    @endslot
+                @endcomponent
+            @endforeach
         </div>
     </div>
-@endsection
