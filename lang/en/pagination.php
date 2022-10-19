76326ccc57a3ba238c66becdcfe8abd4d97209b1
RikkertTheDeveloper <rejarendsen@gmail.com>
2022-10-19 11:24:29 +0200

    Pagination Styling Fixes

--- lang/en/pagination.php
+++ lang/en/pagination.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'previous' => '&laquo; Previous',
-    'next' => 'Next &raquo;',
+    'previous' => 'Vorige',
+    'next' => 'Volgende',
 
 ];
--- public/js/app.js
+++ public/js/app.js
@@ -2183,7 +2183,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
 
 window._ = (lodash__WEBPACK_IMPORTED_MODULE_0___default());
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -2193,18 +2192,14 @@ window._ = (lodash__WEBPACK_IMPORTED_MODULE_0___default());
 
 window.axios = (axios__WEBPACK_IMPORTED_MODULE_1___default());
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
  * allows your team to easily build robust real-time web applications.
  */
-
 // import Echo from 'laravel-echo';
-
 // import Pusher from 'pusher-js';
 // window.Pusher = Pusher;
-
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
 //     key: import.meta.env.VITE_PUSHER_APP_KEY,
