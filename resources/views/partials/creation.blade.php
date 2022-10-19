a73094aed8daa7952cb819a81cee2c439517d395
Jurriën <jurharte@gmail.com>
2022-10-19 14:09:04 +0200

    Icons

--- resources/views/partials/creation.blade.php
+++ resources/views/partials/creation.blade.php
@@ -1,5 +1,5 @@
 @if(\Illuminate\Support\Facades\Auth::user()->role_id >= 2)
-<div class="pagination-box mr-10 ml-10">
-    <a href="/create" class="btn-custom">Event maken</a>
+<div class="w-2 h-2 bg-gray-300 font-bold text-lg mr-10 ml-10">
+    <a href="/create" class="btn-custom">+</a>
 </div>
 @endif
