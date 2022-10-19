a910502b5bac83e182a574d26b010e182bb3c875
Jurriën <jurharte@gmail.com>
2022-10-19 09:34:43 +0200

    changing migration

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -13,9 +13,7 @@ class EventController extends Controller
 {
 
     /**
-        * This function returns a view with all the events within the system.
-        * @return View - A view passed alongside a collection of the events.
-        * @see \Illuminate\Database\Eloquent\Collection
+        * Deze functie geeft een overzicht van alle evenementen terug.
     */
     public function index() {
         $allEvents = Event::All();
@@ -25,18 +23,12 @@ class EventController extends Controller
         ]);
     }
 
-    /**
-        * This function return the event modal alongside the wildcard of the event.
-        * @see Event
-    */
     public function event() {
         return view('event-modal');
     }
 
     /**
-        * This function adds a new event to the database,
-        * @see Event
-        * @implnote - The request is used as a wildcard used in @link routes/web.php
+        * Deze functie maakt een nieuw evenement aan.
     */
     public function store() {
         $newEvent = new Event();
@@ -55,10 +47,9 @@ class EventController extends Controller
             $newEvent->has_food = '0';
          }
 
-         $newEvent->cost = request('price');
-         $newEvent->max_participants = request('maxParticipants');
-         $newEvent->min_people = request('minParticipant');
-         $newEvent->max_people = request('maxParticipants');
+        $newEvent->cost = request('price');
+        $newEvent->min_people = request('minParticipant');
+        $newEvent->max_people = request('maxParticipants');
         $newEvent->color = request('color');
         $newEvent->necessities = request('description');
         $newEvent->image = '1';
@@ -68,11 +59,6 @@ class EventController extends Controller
         return redirect('/');
     }
 
-    /**
-        * This function deletes an event from the database.
-        * @see Event
-        * @implnote - The $id parameter is supplied in the route itself.
-    */
     public function delete($id) {
        $event = Event::all()->where('id', $id)->first();
 
@@ -83,13 +69,8 @@ class EventController extends Controller
         return redirect('/');
     }
 
-    /**
-        * This function edits an existing event that is inside of the database.
-        * @see Event
-        * @return Redirect - When editing has been completed.
-        * @implnote - The request is used as a wildcard used in @link routes/web.php
-    */
     public function edit() {
+//        $userId = Auth::user()['id'];
         $foundEvent = Event::all()->where('id', request('id'))->first();
 
         $foundEvent->user_id = Auth::user()['id'];
@@ -104,18 +85,13 @@ class EventController extends Controller
         $foundEvent->max_participants = request('max_participants') ?: $foundEvent['max_participants'];
         $foundEvent->has_food = request('has_food') ?: $foundEvent['has_food'];
         $foundEvent->image = request('image') ?: $foundEvent['image'];
-        $foundEvent->min_people = request('minParticipant') ?: $foundEvent['min_people'];
-        $foundEvent->max_people = request('maxParticipants') ?: $foundEvent['max_people'];
+        $foundEvent->min_people = request('min_people') ?: $foundEvent['min_people'];
+        $foundEvent->max_people = request('max_people') ?: $foundEvent['max_people'];
 
         $foundEvent->save();
-        return redirect('/events/'.request('id').'/edit');
+        return redirect('/');
     }
 
-    /**
-        * This function returns the editing modal alongside the data that is required.
-        * @return View - A view alongside the required information.
-        * @throws \Symfony\Component\HttpKernel\Exception\HttpException - If the event does not exist.
-    */
     public function editing($eventID) {
         $foundEvent = Event::all()->find($eventID);
         $Event = isset($foundEvent);
@@ -130,26 +106,10 @@ class EventController extends Controller
         }
     }
 
-    public function details($id) {
-        $foundEvent = Event::all()->where('id', $id)->first();
-
-        return view('event-details',[
-            'event' => $foundEvent,
-        ]);
-    }
-
-    /**
-        * This function returns all the events associated with a specific user.
-        * @return Collection - A collection with all the events.
-    */
     public function getEventsWithUser($userID) {
         return Event::all()->where('user_id', $userID);
     }
 
-    /**
-        * This function returns all the reservation associated with an event.
-        * @return Collection - A collection with all the reservations.
-    */
     public function getReservations($eventID) {
         return Reservation::all()->where('event_id', $eventID);
     }
